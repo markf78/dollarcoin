0dc575f7217532bf373bf875cd8c665314d8c04d
markf78 <markf78@yahoo.com>
2013-12-13 14:09:18 -0800

    Add copyright info.

--- src/leveldbwrapper.cpp
+++ src/leveldbwrapper.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2012-2013 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
+// Copyright (c) 2013-2014 The Dollarcoin developers
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "leveldbwrapper.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2013 The Bitcoin developers
+// Copyright (c) 2013-2014 The Dollarcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -28,7 +29,7 @@ using namespace std;
 using namespace boost;
 
 #if defined(NDEBUG)
-# error "Bitcoin cannot be compiled without assertions."
+# error "Dollarcoin cannot be compiled without assertions."
 #endif
 
 //
@@ -65,7 +66,7 @@ map<uint256, set<uint256> > mapOrphanTransactionsByPrev;
 // Constant stuff for coinbase transactions we create:
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "Bitcoin Signed Message:\n";
+const string strMessageMagic = "Dollarcoin Signed Message:\n";
 
 // Settings
 int64_t nTransactionFee = 0;
@@ -1504,7 +1505,7 @@ bool FindUndoPos(CValidationState &state, int nFile, CDiskBlockPos &pos, unsigne
 static CCheckQueue<CScriptCheck> scriptcheckqueue(128);
 
 void ThreadScriptCheck() {
-    RenameThread("bitcoin-scriptch");
+    RenameThread("dollarcoin-scriptch");
     scriptcheckqueue.Thread();
 }
 
